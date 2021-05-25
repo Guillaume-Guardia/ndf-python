dad7e39aac09279d3d985b253a38cb64ece63be2
Guillaume Guardia <guardia.1903@apside-groupe.com>
2021-05-25 14:43:48 +0200

    Update

--- src/pyndf/gui/tables/useclass/pdf.py
+++ src/pyndf/gui/tables/useclass/pdf.py
@@ -7,7 +7,6 @@ from pyndf.gui.dialogs.preview import PreviewDialog
 class PdfTable(AnalyseTable):
     def finished(self):
         self.cellClicked.connect(self.on_filename_clicked)
-        self.tab.window.toggled_tab(self.tab, True)
         super().finished()
 
     def on_filename_clicked(self, row, col):
