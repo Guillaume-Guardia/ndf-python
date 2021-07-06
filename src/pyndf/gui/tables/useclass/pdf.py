ed7a2481666b151a3c4914c3ed5a4d0449d1ad06
Guillaume Guardia <guardia.1903@apside-groupe.com>
2021-07-06 10:27:10 +0200

    Hide column if all status are good.

--- src/pyndf/gui/tables/useclass/pdf.py
+++ src/pyndf/gui/tables/useclass/pdf.py
@@ -21,6 +21,7 @@ class PdfTable(AnalyseTable):
         column = self.columnCount() - 1
 
         # Add button to row with a red status
+        must_hide_retry_col = True
         for row in range(self.rowCount()):
             if self.item(row, self.custom_item.headers.index("status")).status:
                 continue
@@ -30,6 +31,12 @@ class PdfTable(AnalyseTable):
             )
             button = RetryButton(matricule, self)
             self.setCellWidget(row, column, button)
+            must_hide_retry_col = False
+
+        self.setColumnHidden(
+            self.custom_item.headers.index("retry"),
+            must_hide_retry_col,
+        )
 
         # Add the total column and set visible the corresponding item in menu
         super().finished()
